ba22a6fa00fbd5cc1f6be8c77a39fef1fa72229f
EllenLis <elena139@bk.ru>
2022-10-27 21:55:06 +0300

    Initial Commit

---
+++ CollectionView_Lysenskaia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView_Lysenskaia
+//
+//  Created by ElenaL on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
